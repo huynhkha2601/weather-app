846ae88dac7eb9a33d1993353795c8013d40deff
huynhkha2601 <63405972+huynhkha2601@users.noreply.github.com>
2022-07-18 11:43:00 +0700

    Changes index

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require("request");
 const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-    address +
+    encodeURIComponent(address) +
     ".json?access_token=pk.eyJ1IjoiaHV5bmhraGEyNjAxZ2dhZG0iLCJhIjoiY2w0dG1tdnFmMDhuajNldDBjaDRieTdwNyJ9.9EpVRbEabYHAKOZ8gMPOng&limit=1";
 
   request({ url, json: true }, (error, { body }) => {
